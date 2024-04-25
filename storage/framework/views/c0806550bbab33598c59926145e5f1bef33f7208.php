620e60639a027697fe34b94bb1b0d84d0212ffaa
Mohammad TRABELSI <mohammad.trabelsi@gmail.com>
2024-04-25 10:58:55 +0100

    delete from storage

--- storage/framework/views/c0806550bbab33598c59926145e5f1bef33f7208.php
+++
@@ -1,5 +0,0 @@
-
-<body>
-
-<!-- <body data-layout="horizontal"> -->
-<?php /**PATH /var/www/vhosts/2earn.cash/dev.2earn.cash/resources/views/layouts/body.blade.php ENDPATH**/ ?>
--- storage/framework/views/c565b7531a53385c51258c21bb82f476a449e610.php
+++
@@ -1,6 +0,0 @@
-<div>
-    <?php $__env->startComponent('components.breadcrumb'); ?>
-        <?php $__env->slot('title'); ?> <?php echo e(__('Historique_recuperation')); ?> <?php $__env->endSlot(); ?>
-    <?php echo $__env->renderComponent(); ?>
-</div>
-<?php /**PATH C:\wamp64\www\2earn\resources\views/livewire/historique-recuperation.blade.php ENDPATH**/ ?>
