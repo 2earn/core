b61419768ca66920b4a6a2eb4a1162d0d256f0b1
Mohammad TRABELSI <mohammad.trabelsi@gmail.com>
2024-05-02 12:01:26 +0100

    Add escape html spec chars code

--- resources/views/components/page-title.blade.php
+++ resources/views/components/page-title.blade.php
@@ -1,7 +1,6 @@
 <div class="row">
     <div class="col-12">
-        <div class="page-title-box d-sm-flex align-items-center justify-content-between"  @if (!is_null($bg))            style="background-color: {{$bg}} !important;"
-        @endif >
+        <div class="page-title-box d-sm-flex align-items-center justify-content-between"  @if (!is_null($bg)) style="background-color: {{$bg}} !important;"  @endif >
             <h4 class="mb-sm-0 font-size-18">{!! $pageTitle !!}</h4>
         </div>
     </div>
