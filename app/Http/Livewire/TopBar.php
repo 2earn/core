e168b6fd50afbd731b996ff51339d68a7f73d0a9
Mohammad TRABELSI <mohammad.trabelsi@gmail.com>
2024-05-02 12:25:29 +0100

    update top bar remove commented code / fix role translation and intval

--- app/Http/Livewire/TopBar.php
+++ app/Http/Livewire/TopBar.php
@@ -1,6 +1,6 @@
 <?php
-
 namespace App\Http\Livewire;
+
 use Core\Models\language;
 use Core\Services\BalancesManager;
 use Core\Services\settingsManager;
@@ -8,7 +8,6 @@ use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Lang;
 use Livewire\Component;
-
 
 class TopBar extends Component
 {
@@ -23,51 +22,26 @@ class TopBar extends Component
     {
         $this->settingsManager = $settingsManager;
         $this->balancesManager = $balancesManager;
-//        App::setLocale(app()->getLocale()) ;
-//          dd(App::getLocale());
-        $dd = Lang::get('Edit');
-        //  dd($dd);
     }
 
     public function render(settingsManager $settingsManager)
     {
-
         $user = $settingsManager->getAuthUser();
         $userById = $settingsManager->getUserById($user->id);
-       $userRole = $userById->getRoleNames()->first();
-        $notif = $settingsManager->getNomberNotification();
-
         if (!$user)
             dd('not found page');
-        $solde = $this->balancesManager->getBalances($user->idUser);
-
-        return view('livewire.top-bar',
-            [
-                'solde' => $solde,
-                'user' => $user,
-                'notificationbr' => $notif,
-                'userRole'=>$userRole
-            ]
-        );
+        $params = [
+            'solde' => $this->balancesManager->getBalances($user->idUser),
+            'user' => $user,
+            'notificationbr' => $settingsManager->getNomberNotification(),
+            'userRole' => $userById->getRoleNames()->first()
+        ];
+        return view('livewire.top-bar', $params);
     }
 
     public function logout(settingsManager $settingsManager)
     {
-//        redirect()->route('home', app()->getLocale());
         $settingsManager->logoutUser();
-
-//        dd('sd');
-//        return redirect()->route('login', app()->getLocale())->with('message', Lang::get('your phone or your password is incorrect !'));
-
-        return redirect()->route('login',['locale'=>app()->getLocale()])->with('FromLogOut', 'FromLogOut');
+        return redirect()->route('login', ['locale' => app()->getLocale()])->with('FromLogOut', 'FromLogOut');
     }
-
-
-
-
-
-
-
-
-
 }
