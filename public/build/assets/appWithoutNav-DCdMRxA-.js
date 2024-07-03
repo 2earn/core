9fd990d030ff66157793de4fc40c4cb0e2224203
Mohammad <mohammad.trabelsi@gmail.com>
2024-07-03 12:20:38 +0100

    update git ignore remove vite

--- public/build/assets/appWithoutNav-DCdMRxA-.js
+++
@@ -1,1 +0,0 @@
-import{j as t,b as o,S as w,m as a,i}from"./intlTelInput-rztKayHV.js";Object.assign(window,{$:t,jQuery:t});window.bootstrap=o;window.Swal=w;window.Alpine=a;a.start();window.intlTelInput=i;
