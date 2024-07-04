d12e0fad0f334c99a00c8f7cbbf090614fc5f6c5
Mohammad <mohammad.trabelsi@gmail.com>
2024-07-04 09:19:19 +0100

    remove service provider telescope

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,10 +15,6 @@ class AppServiceProvider extends ServiceProvider
      */
         public function register(): void
     {
-        if ($this->app->environment('local')) {
-            $this->app->register(\Laravel\Telescope\TelescopeServiceProvider::class);
-            $this->app->register(TelescopeServiceProvider::class);
-        }
         $this->app->bind('Sponsorship', function ($app) {
             return new Sponsorship($app->make('App\DAL\UserRepository'),$app->make('Core\Services\BalancesManager'));
         });
