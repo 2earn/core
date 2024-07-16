978a5f1dc035516c3d1113546749ee2035e90c8c
Mohammad <mohammad.trabelsi@gmail.com>
2024-07-16 14:21:38 +0100

    Revert routes

--- routes/web.php
+++ routes/web.php
@@ -110,7 +110,7 @@ Route::group(['prefix' => '{locale}', 'where' => ['locale' => '[a-zA-Z]{2}'], 'm
         Route::middleware(['IsSuperAdmin'])->group(function () {
             Route::get('/user_list', \App\Http\Livewire\UsersList::class)->name('user_list');
             Route::get('/configuration-ha', ConfigurationHA::class)->name('configuration-ha');
-            Route::get('/configuration-setting', \App\Http\Livewire\ConfigurationSetting::class)->name('configuration_setting');
+            Route::get('/configuration-setting', \App\Http\Livewire\ConfigurationSetting::class)->name('configuration-setting');
             Route::get('/configuration-bo', \App\Http\Livewire\ConfigurationBO::class)->name('configuration-bo');
             Route::get('/configuration-amounts', \App\Http\Livewire\ConfigurationAmounts::class)->name('configuration-amounts');
             Route::get('/edit_admin', \App\Http\Livewire\EditAdmin::class)->name('edit_admin');
