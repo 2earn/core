fc857c8be51408f19d5f3f8763142bf10f564375
Mohammad <mohammad.trabelsi@gmail.com>
2024-08-12 08:27:22 +0100

    Fix rtl inter national id

--- resources/views/livewire/identification-check.blade.php
+++ resources/views/livewire/identification-check.blade.php
@@ -180,7 +180,7 @@
                                                 <th scope="row">
                                                     {{__('Email identificatdion modal')}}
                                                 </th>
-                                                <td>
+                                                <td class="forceltr">
                                                     {{$userF['email']}}
                                                 </td>
                                             </tr>
@@ -359,7 +359,7 @@
                                                 {{ __('InternationalId ID identificatdion modal') }}
                                                 <span class="text-danger">*</span>
                                             </label>
-                                            <input type="text" class="form-control" minlength="5" maxlength="50"
+                                            <input type="text" class="form-control forceltr" minlength="5" maxlength="50"
                                                    wire:model.defer="userF.internationalID"
                                                    id="internationalId" placeholder="{{__('InternationalId ID')}}">
                                         </div>
