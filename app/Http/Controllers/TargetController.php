c5f1ac60f679e25ca007c1e678a1c01ab6aec158
Mohammad <mohammad.trabelsi@gmail.com>
2024-08-23 13:08:43 +0100

    Link details users

--- app/Http/Controllers/TargetController.php
+++ app/Http/Controllers/TargetController.php
@@ -4,11 +4,17 @@ namespace App\Http\Controllers;
 
 use App\Models\Target;
 use App\Services\Targeting\Targeting;
+use Illuminate\Support\Facades\Lang;
 
 class TargetController extends Controller
 {
     public function getTargetData($idTarget)
     {
-        return datatables(Targeting::getTargetQuery(Target::find($idTarget), true))->make(true);
+        return datatables(Targeting::getTargetQuery(Target::find($idTarget), true))
+            ->addColumn('detail', function ($query) {
+                return '<a target=”_blank” href="' . route('user_details', ['locale' => app()->getLocale(), 'idUser' => $query->id]) . '">' . Lang::get('See Details') . '</a>';
+            })
+            ->rawColumns(['detail'])
+            ->make(true);
     }
 }
