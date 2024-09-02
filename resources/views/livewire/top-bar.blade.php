d82936c4cbc0d66174f26dce7bf04fc72073f0c6
Mohammad <mohammad.trabelsi@gmail.com>
2024-09-02 12:36:34 +0100

    update light dark mode

--- resources/views/livewire/top-bar.blade.php
+++ resources/views/livewire/top-bar.blade.php
@@ -114,21 +114,19 @@
                         <div class="dropdown-menu dropdown-menu-end">
                             @foreach (config('app.available_locales') as  $locale => $value )
                                 @if(strpos(Request::url(), '/'.app()->getLocale().'/'))
-                                    <a
-                                        href="{{str_replace('/'.app()->getLocale().'/', '/'.$value['name'].'/', Request::url())}}"
-                                        class="dropdown-item notify-item language py-2  @if($locale==app()->getLocale()) active @endif"
-                                        data-lang="{{$locale}}"
-                                        title="{{ __('lang'.$locale)  }}" data-turbolinks="false">
+                                    <a href="{{str_replace('/'.app()->getLocale().'/', '/'.$value['name'].'/', Request::url())}}"
+                                       class="dropdown-item notify-item language py-2  @if($locale==app()->getLocale()) active @endif"
+                                       data-lang="{{$locale}}"
+                                       title="{{ __('lang'.$locale)  }}" data-turbolinks="false">
                                         <img src="{{ Vite::asset('resources/images/flags/'.$value['flag'].'.svg') }}"
                                              alt="user-image" class="me-2 rounded" height="20">
                                         <span class="align-middle">{{ __('lang'.$locale)  }}</span>
                                     </a>
                                 @else
-                                    <a
-                                        href="{{str_replace('/'.app()->getLocale(), '/'.$value['name'].'/', Request::url())}}"
-                                        class="dropdown-item notify-item language py-2  @if($locale==app()->getLocale()) active @endif"
-                                        data-lang="{{$locale}}"
-                                        title="{{ __('lang'.$locale)  }}" data-turbolinks="false">
+                                    <a href="{{str_replace('/'.app()->getLocale(), '/'.$value['name'].'/', Request::url())}}"
+                                       class="dropdown-item notify-item language py-2  @if($locale==app()->getLocale()) active @endif"
+                                       data-lang="{{$locale}}"
+                                       title="{{ __('lang'.$locale)  }}" data-turbolinks="false">
                                         <img src="{{ Vite::asset('resources/images/flags/'.$value['flag'].'.svg') }}"
                                              alt="user-image" class="me-2 rounded" height="20">
                                         <span class="align-middle">{{ __('lang'.$locale)  }}</span>
