887ea51459528a0fe09a40f1422632d3f30e4a29
Mohammad <mohammad.trabelsi@gmail.com>
2024-09-17 17:06:17 +0100

    edit user contact

--- resources/views/livewire/edit-user-contact.blade.php
+++ resources/views/livewire/edit-user-contact.blade.php
@@ -1,21 +1,17 @@
 <div>
+    @component('components.breadcrumb')
+        @slot('title')
+            {{ __('Edit contact') }}
+        @endslot
+    @endcomponent
     <div class="card">
         <div class="card-header">
             <h5 class="card-title" id="ContactsModalLabel">{{ __('Edit a contact') }}</h5>
         </div>
         <div class="card-body ">
             <div class="row">
-                <div class="row">
-                    @if(Session::has('success'))
-                        <div class="alert alert-success material-shadow" role="alert">
-                            {{ Session::get('success') }}
-                        </div>
-                    @endif
-                    @if(Session::has('danger'))
-                        <div class="alert alert-danger material-shadow" role="alert">
-                            {{ Session::get('danger') }}
-                        </div>
-                    @endif
+                <div class="col-12">
+                    @include('layouts.flash-messages')
                 </div>
             </div>
             <div class="row">
@@ -33,7 +29,7 @@
                         </div>
                         <div class="col-md-4">
                             <div class="form-group">
-                                <label class="form-label">{{ __('Last Name') }}</label>
+                                <label class="form-label">{{ __('Last name') }}</label>
                                 <input id="inputlLastNameContact" type="text"
                                        class="form-control" name="inputlLastNameContact"
                                        wire:model.defer="lastNameUserContact"
@@ -59,10 +55,12 @@
                             <input type="text" name="idUser" hidden>
                             <div class="col-md-12">
                                 <button type="button" class="btn btn-outline-secondary"
+                                <button type="button" class="btn btn-outline-secondary float-end  mx-1"
                                         wire:click="close">{{ __('Close') }}
                                 </button>
                                 <button type="button" id="SubmitAd3dContact" onclick="editContactEvent()"
                                         class="btn btn-outline-info">
+                                    class="btn btn-outline-info float-end mx-1">
                                     {{ __('Save') }}
                                     <div wire:loading wire:target="save">
                                             <span class="spinner-border spinner-border-sm" role="status"
@@ -91,3 +89,4 @@
         }
     </script>
 </div>
+
