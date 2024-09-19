4eb9a48e6425bbf9f223bc4d9ec9cfe6e2840959
Mohammad <mohammad.trabelsi@gmail.com>
2024-09-19 14:17:06 +0100

    Add pages

---
+++ resources/views/livewire/sensory-representation-system.blade.php
@@ -0,0 +1,18 @@
+<div>
+    @section('title')
+        {{ __('Sensory Representation System') }}
+    @endsection
+    @component('components.breadcrumb')
+        @slot('li_1')@endslot
+        @slot('title')
+            {{ __('Sensory Representation System') }}
+        @endslot
+    @endcomponent
+
+    <div class="card">
+        <div class="card-body">
+
+        </div>
+    </div>
+
+</div>
