11a35a9e6b7a622307308e43761aff52c34f81fe
Kheireddine Guidara <guidara.kheireddine@iit.ens.tn>
2024-10-29 16:18:58 +0100

    Adapt it for little and mobile  screen

--- resources/views/livewire/static-news.blade.php
+++ resources/views/livewire/static-news.blade.php
@@ -8,14 +8,14 @@
                             class="trending-ribbon-text">{{__('News')}}</span>
                     </div>
                     <div class="row">
-                        <div class="col-2">
+                        <div class="col-12 col-md-2 mb-3 mb-md-0">
                             <div class="d-flex justify-content-center align-items-center w-100 h-100">
                                 <img src="{{ Vite::asset('resources/images/WhatsApp.jpg') }}"
                                      alt="Description de l'image"
                                      class="img-thumbnail img-fluid w-100 h-100" style="object-fit: cover;">
                             </div>
                         </div>
-                        <div class="col">
+                        <div class="col-12 col-md">
                             <blockquote class="card-blockquote mb-0">
                                 <p class="text-muted mb-2">
                                     @if(app()->getLocale()=="en")
@@ -28,27 +28,18 @@
                                 </p>
                                 <p class="text-muted mb-2">
                                     @if(app()->getLocale()=="en")
-                                        A private and exclusive WhatsApp group has been created for people registered on
-                                        the
-                                        2earn.cash platform... Thank you to all other members who have not yet joined
-                                        the group
-                                        to click on <a href="https://chat.whatsapp.com/JRJV7LgsULwGyvIsvvXm9A">this
-                                            link</a> to send a membership request.
-
+                                        A private and exclusive WhatsApp group has been created for people registered on the
+                                        2earn.cash platform... Thank you to all other members who have not yet joined the group
+                                        to click on <a href="https://chat.whatsapp.com/JRJV7LgsULwGyvIsvvXm9A">this link</a> to send a membership request.
                                     @elseif(app()->getLocale()=="fr")
-                                        Un groupe WhatsApp privé et exclusif a été créé pour les personnes inscrites sur
-                                        la
-                                        plateforme 2earn.cash... Merci à tous les autres membres qui n'ont pas encore
-                                        rejoint le
-                                        groupe de cliquer sur <a
-                                            href="https://chat.whatsapp.com/JRJV7LgsULwGyvIsvvXm9A">ce lien</a> pour
-                                        envoyer une demande d'adhésion.
+                                        Un groupe WhatsApp privé et exclusif a été créé pour les personnes inscrites sur la
+                                        plateforme 2earn.cash... Merci à tous les autres membres qui n'ont pas encore rejoint le
+                                        groupe de cliquer sur <a href="https://chat.whatsapp.com/JRJV7LgsULwGyvIsvvXm9A">ce lien</a> pour envoyer une demande d'adhésion.
                                     @else
-                                        تم إنشاء مجموعة واتساب خاصة و حصرية للمسجلين في منصة 2earn.cash ...  الرجاء من
+                                        تم إنشاء مجموعة واتساب خاصة و حصرية للمسجلين في منصة 2earn.cash ... الرجاء من
                                         كل الأخوة الأعضاء الذين لم يلتحقوا بعد بالمجموعة النقر على
-                                        <a href="https://chat.whatsapp.com/JRJV7LgsULwGyvIsvvXm9A"> هذا الرابط </a>
-                                        لإرسال طلب إنظمام
-
+                                        <a href="https://chat.whatsapp.com/JRJV7LgsULwGyvIsvvXm9A">هذا الرابط</a>
+                                        لإرسال طلب إنضمام
                                     @endif
                                 </p>
                                 <p class="text-muted mb-2">
@@ -58,10 +49,12 @@
                                         Cordialement
                                     @else
                                         أطيب التحيات
-                                    @endif</p>
+                                    @endif
+                                </p>
                             </blockquote>
                         </div>
                     </div>
+
                     <blockquote class="card-blockquote mb-0 float-end">
                         <p class="text-info mb-2">
                             @if(app()->getLocale()=="en")
