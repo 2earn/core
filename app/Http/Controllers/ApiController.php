601d7797102d35deedefdcd4f8c5c66717244f72
Mohammad <mohammad.trabelsi@gmail.com>
2024-11-20 09:42:07 +0100

    update get_user_balances_list

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -992,13 +992,7 @@ class="btn btn-xs btn-primary edit-amounts-btn btn2earnTable"  >
 
     public function getUserBalancesList($locale, $idUser, $idamount)
     {
-
-        $userData = DB::select("select ref,u.idUser,u.idamount,Date,u.idBalancesOperation,b.operation,u.Description,
- case when b.IO ='I' then value else -value end value ,u.balance from user_balances u,balance_operations b,users s
-where u.idBalancesOperation=b.id
-  and u.idUser=s.idUser
-and u.idamount not in(4,6)  and u.idUser=? and u.idamount=? order by Date   ", [$idUser, $idamount]
-        );
+        $userData = DB::select(getSqlFromPath('get_user_balances_list'), [$idUser, $idamount]        );
         return response()->json($userData);
     }
 
