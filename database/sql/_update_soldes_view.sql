4562df406aa143263e9f542ae1d2a9b0e482c7d8
Mohammad <mohammad.trabelsi@gmail.com>
2024-11-22 09:17:22 +0100

    update soldeview

--- database/sql/_update_soldes_view.sql
+++ database/sql/_update_soldes_view.sql
@@ -3,76 +3,76 @@ SELECT
     `users`.`id` AS `id`,
     (SELECT
          IFNULL(SUM(`u`.`value` * CASE
-                                      WHEN `b`.`IO` = 'I' THEN 1
+                                      WHEN `b`.`io` = 'I' THEN 1
                                       ELSE - 1
              END),
                 0) AS `solde`
      FROM
          (`user_balances` `u`
-             JOIN `balanceoperations` `b`)
+             JOIN `balance_operations` `b`)
      WHERE
-         `u`.`idBalancesOperation` = `b`.`idBalanceOperations`
-       AND `b`.`MODIFY_AMOUNT` = '1'
+         `u`.`idBalancesOperation` = `b`.`id`
+       AND `b`.`modify_amount` = '1'
        AND `users`.`idUser` = `u`.`idUser`
        AND `u`.`idamount` = 1
      GROUP BY `u`.`idUser` , `u`.`idamount`) AS `cash`,
     (SELECT
          IFNULL(SUM(`u`.`value` * CASE
-                                      WHEN `b`.`IO` = 'I' THEN 1
+                                      WHEN `b`.`io` = 'I' THEN 1
                                       ELSE - 1
              END),
                 0) AS `solde`
      FROM
          (`user_balances` `u`
-             JOIN `balanceoperations` `b`)
+             JOIN `balance_operations` `b`)
      WHERE
-         `u`.`idBalancesOperation` = `b`.`idBalanceOperations`
-       AND `b`.`MODIFY_AMOUNT` = '1'
+         `u`.`idBalancesOperation` = `b`.`id`
+       AND `b`.`modify_amount` = '1'
        AND `users`.`idUser` = `u`.`idUser`
        AND `u`.`idamount` = 2
      GROUP BY `u`.`idUser` , `u`.`idamount`) AS `bfs`,
     (SELECT
          IFNULL(SUM(`u`.`value` * CASE
-                                      WHEN `b`.`IO` = 'I' THEN 1
+                                      WHEN `b`.`io` = 'I' THEN 1
                                       ELSE - 1
              END),
                 0) AS `solde`
      FROM
          (`user_balances` `u`
-             JOIN `balanceoperations` `b`)
+             JOIN `balance_operations` `b`)
      WHERE
-         `u`.`idBalancesOperation` = `b`.`idBalanceOperations`
-       AND `b`.`MODIFY_AMOUNT` = '1'
+         `u`.`idBalancesOperation` = `b`.`id`
+       AND `b`.`modify_amount` = '1'
        AND `users`.`idUser` = `u`.`idUser`
        AND `u`.`idamount` = 3
      GROUP BY `u`.`idUser` , `u`.`idamount`) AS `db`,
     (SELECT
          IFNULL(SUM(`u`.`value` * CASE
-                                      WHEN `b`.`IO` = 'I' THEN 1
+                                      WHEN `b`.`io` = 'I' THEN 1
                                       ELSE - 1
              END),
                 0) AS `solde`
      FROM
          (`user_balances` `u`
-             JOIN `balanceoperations` `b`)
+             JOIN `balance_operations` `b`)
      WHERE
-         `u`.`idBalancesOperation` = `b`.`idBalanceOperations`
-       AND `b`.`MODIFY_AMOUNT` = '1'
+         `u`.`idBalancesOperation` = `b`.`id`
+       AND `b`.`modify_amount` = '1'
        AND `users`.`idUser` = `u`.`idUser`
        AND `u`.`idamount` = 4
      GROUP BY `u`.`idUser` , `u`.`idamount`) AS `t`,
     (SELECT
          IFNULL(SUM(`u`.`value` * CASE
-                                      WHEN `b`.`IO` = 'I' THEN 1
+                                      WHEN `b`.`io` = 'I' THEN 1
                                       ELSE - 1
              END),
                 0) AS `solde`
      FROM
          (`user_balances` `u`
-             JOIN `balanceoperations` `b`)
+             JOIN `balance_operations` `b`)
      WHERE
-         `u`.`idBalancesOperation` = `b`.`idBalanceOperations`
-       AND `b`.`MODIFY_AMOUNT` = '1'
+         `u`.`idBalancesOperation` = `b`.`id`
+       AND `b`.`modify_amount` = '1'
        AND `users`.`idUser` = `u`.`idUser`
        AND `u`.`idamount` = 5
      GROUP BY `u`.`idUser` , `u`.`idamount`) AS `sms`,
