d4b88758b7bd18e02ad62660e046f5ac365a189b
Mohammad <mohammad.trabelsi@gmail.com>
2024-12-06 15:46:12 +0100

    Convert sql lot 3

--- app/DAL/HistoryNotificationRepository.php
+++ app/DAL/HistoryNotificationRepository.php
@@ -19,25 +19,122 @@ class  HistoryNotificationRepository implements IHistoryNotificationRepository
     public function getHistoryForModerateur()
     {
 
-        return DB::select(getSqlFromPath('history_notification_repository'));
+        $historyNotSystem = DB::table('history')
+            ->select(
+                'history.ref as reference',
+                DB::raw('IFNULL(usend.name, "System") as send'),
+                'users.name as receiver',
+                DB::raw('IFNULL(title, "") as action'),
+                'history.date',
+                'history.type',
+                DB::raw('IFNULL(history.reponce, "") as responce')
+            )
+            ->join('users', 'history.id_reciver', '=', 'users.idUser')
+            ->leftJoin('users as usend', 'history.id_send', '=', 'usend.idUser')
+            ->leftJoin('action_history', 'history.id_action', '=', 'action_history.id')
+            ->where('history.id_reciver', '!=', "11111111");
+
+        $historySystem = DB::table('history')
+            ->select(
+                'history.ref as reference',
+                DB::raw('IFNULL(users.name, " ") as send'),
+                DB::raw('"System" as receiver'),
+                DB::raw('IFNULL(title, "") as action'),
+                'history.date',
+                'history.type',
+                DB::raw('IFNULL(history.reponce, "") as responce')
+            )
+            ->join('users', 'history.id_send', '=', 'users.idUser')
+            ->leftJoin('action_history', 'history.id_action', '=', 'action_history.id')
+            ->where('history.id_reciver', '=', "11111111");
+
+        return $historyNotSystem->unionAll($historySystem)->get();
     }
 
+    public function getHistoryNotificationModerator()
+    {
+
+        $historyNotSystem = DB::table('history')
+            ->select(
+                'history.ref as reference',
+                DB::raw('IFNULL(usend.name, "System") as send'),
+                'users.name as receiver',
+                DB::raw('IFNULL(title, "") as action'),
+                'history.date',
+                'history.type',
+                DB::raw('IFNULL(history.reponce, "") as responce')
+            )
+            ->join('users', 'history.id_reciver', '=', 'users.idUser')
+            ->leftJoin('users as usend', 'history.id_send', '=', 'usend.idUser')
+            ->leftJoin('action_history', 'history.id_action', '=', 'action_history.id')
+            ->where('history.id_reciver', '!=', "11111111");
+
+        $historySystem = DB::table('history')
+            ->select(
+                'history.ref as reference',
+                DB::raw('IFNULL(users.name, " ") as send'),
+                DB::raw('"System" as receiver'),
+                DB::raw('IFNULL(title, "") as action'),
+                'history.date',
+                'history.type',
+                DB::raw('IFNULL(history.reponce, "") as responce')
+            )
+            ->join('users', 'history.id_send', '=', 'users.idUser')
+            ->leftJoin('action_history', 'history.id_action', '=', 'action_history.id')
+            ->where('history.id_reciver', '=', "11111111");
+
+        return $historyNotSystem->unionAll($historySystem)->get();
+
+    }
+
+    public function getHistoryNotificationAdmin()
+    {
+        return DB::table('history')
+            ->select(
+                'history.ref as reference',
+                DB::raw('IFNULL(usend.name,                "System") as send'),
+                'users.name as receiver',
+                DB::raw('IFNULL(title, "") as action'),
+                'history.date', 'history.type',
+                DB::raw('IFNULL(history.reponce, "") as responce')
+            )
+            ->join('users', 'history.id_reciver', '=', 'users.idUser')
+            ->leftJoin('users as usend', 'history.id_send', '=', 'usend.idUser')
+            ->leftJoin('action_history', 'history.id_action', '=', 'action_history.id')
+            ->get();
+    }
+
+    public function getHistoryNotificationUser($receiverId)
+    {
+        $results = DB::table('history')->select(
+            'history.ref as reference',
+            DB::raw('IFNULL(usend.name, "System") as send'),
+            'users.name as receiver', DB::raw('IFNULL(title, "") as action'),
+            'history.date', 'history.type', DB::raw('IFNULL(history.reponce, "") as responce')
+        )
+            ->join('users', 'history.id_reciver', '=', 'users.idUser')
+            ->leftJoin('users as usend', 'history.id_send', '=', 'usend.idUser')
+            ->leftJoin('action_history', 'history.id_action', '=', 'action_history.id')
+            ->where('history.id_reciver', $receiverId)->get();
+    }
     public function getHistoryByRole()
     {
+        $result = null;
         switch (strtoupper(auth()->user()->getRoleNames()->first())) {
             case  User::SUPER_ADMIN_ROLE_NAME :
-                return DB::select(getSqlFromPath('history_notification_repository_admin'));
+                $result = $this->getHistoryNotificationAdmin();
                 break;
             case strtoupper("Admin"):
-                return DB::select(getSqlFromPath('history_notification_repository_admin'));
+                $result = $this->getHistoryNotificationAdmin();
                 break;
-            case strtoupper("Moderateur") :
-                return DB::select(getSqlFromPath('history_notification_repository_moderateur'));
+            case strtoupper("Moderateur"):
+                $result = $this->getHistoryNotificationModerator();
                 break;
             case strtoupper("user") :
-                return DB::select(getSqlFromPath('history_notification_repository_user') . auth()->user()->idUser);
+                $result = $this->getHistoryNotificationUser(auth()->user()->idUser);
                 break;
         }
+        return $result;
     }
 
 }
--- app/Http/Livewire/IdentificationRequest.php
+++ app/Http/Livewire/IdentificationRequest.php
@@ -10,7 +10,25 @@ class IdentificationRequest extends Component
 {
     public function render()
     {
-        $identificationRequests = DB::select(getSqlFromPath('identification_request'), [StatusRequest::InProgressNational->value, StatusRequest::InProgressInternational->value, StatusRequest::InProgressGlobal->value]);
+        $identificationRequests = DB::table('identificationuserrequest as ir')
+            ->select(
+                'u1.id as id',
+                'u1.idUser as idUser',
+                'u1.status as status',
+                DB::raw("CONCAT(mu.enFirstName, ' ', mu.enLastName) as enName"),
+                'mu.nationalID as nationalID', 'u1.fullphone_number', 'u1.internationalID',
+                'u1.expiryDate', 'ir.created_at as DateCreation', 'ir.id as irid',
+                'u2.name as Validator', 'ir.response', 'ir.responseDate as DateReponce',
+                'ir.note'
+            )
+            ->join('users as u1', 'ir.IdUser', '=', 'u1.idUser')
+            ->join('metta_users as mu', 'ir.idUser', '=', 'mu.idUser')
+            ->leftJoin('users as u2', 'ir.idUserResponse', '=', 'u2.idUser')
+            ->where(function ($query) {
+                $query->where('ir.status', StatusRequest::InProgressNational->value)
+                    ->orWhere('ir.status', StatusRequest::InProgressInternational->value)
+                    ->orWhere('ir.status', StatusRequest::InProgressGlobal->value);
+            })->get();
         return view('livewire.identification-request', ['identificationRequests' => $identificationRequests])->extends('layouts.master')->section('content');
     }
 }
