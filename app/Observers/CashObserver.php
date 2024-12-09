6d7507dd0523bec16d17c5fadfd7b7115976b10a
Mohammad <mohammad.trabelsi@gmail.com>
2024-12-09 13:39:25 +0100

    add cash

--- app/Observers/CashObserver.php
+++ app/Observers/CashObserver.php
@@ -3,6 +3,9 @@
 namespace App\Observers;
 
 use App\Models\CashBalances;
+use App\Models\UserCurrentBalanceHorisontal;
+use App\Models\UserCurrentBalanceVertical;
+use Core\Enum\BalanceEnum;
 
 class CashObserver
 {
@@ -14,7 +17,29 @@ class CashObserver
      */
     public function created(CashBalances $cashBalances)
     {
-        //
+        $userCurrentBalancehorisontal = UserCurrentBalancehorisontal::where('user_id', $cashBalances->beneficiary_id)->first();
+
+        $newCashBalanceHorisental = $userCurrentBalancehorisontal->cash_balance + $cashBalances->value;
+        $userCurrentBalancehorisontal->update(
+            [
+                'cash_balance' => $newCashBalanceHorisental
+            ]);
+
+        $userCurrentBalanceVertical = UserCurrentBalanceVertical::where('user_id', $cashBalances->beneficiary_id)
+            ->where('balance_id', BalanceEnum::CASH)
+            ->first();
+        $newCashBalanceVertical = $userCurrentBalanceVertical->cash_balance + $cashBalances->value;
+
+        $userCurrentBalanceVertical->update(
+            [
+                'current_balance' => $newCashBalanceVertical,
+                'previous_balance' => $userCurrentBalanceVertical->cash_balance,
+                'last_operation_id' => $cashBalances->id,
+                'last_operation_value' => $cashBalances->value,
+                'last_operation_date' => $cashBalances->created_at,
+            ]
+        );
+
     }
 
     /**
