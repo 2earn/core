99d4c3771eef226e39df13802d10d46be7063a87
Mohammad <mohammad.trabelsi@gmail.com>
2024-12-09 14:53:46 +0100

    Add pool observer logic

---
+++ app/Observers/PoolObserver.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\Pool;
+
+class PoolObserver
+{
+    public function sharePool(Pool $pool)
+    {
+    }
+
+    public function init(Pool $pool)
+    {
+        $pool->value = $pool->value - $pool->max;
+        $pool->save();
+    }
+
+    public function updated(Pool $pool)
+    {
+        if ($pool->value >= $pool->max) {
+            $this->init($pool);
+            $this->sharePool($pool);
+        }
+    }
+
+
+}
