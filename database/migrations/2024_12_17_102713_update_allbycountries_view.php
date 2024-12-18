052b03818da0d5418afebcba9c1ecfe8cc2bc6c9
Mohammad <mohammad.trabelsi@gmail.com>
2024-12-18 15:32:24 +0100

    Fix tableau_croise_view

--- database/migrations/2024_12_17_102713_update_allbycountries_view.php
+++ database/migrations/2024_12_17_102713_update_allbycountries_view.php
@@ -9,13 +9,11 @@ return new class extends Migration
     public function up()
     {
         DB::statement(formatSqlWithEnv(getSqlFromPath('_update_allbycountries_view')));
-        DB::statement(formatSqlWithEnv(getSqlFromPath('tableau_croise_view')));
     }
 
 
     public function down()
     {
         DB::statement('DROP VIEW IF EXISTS allbycountries;');
-        DB::statement('DROP VIEW IF EXISTS tableau_croise_view;');
     }
 };
