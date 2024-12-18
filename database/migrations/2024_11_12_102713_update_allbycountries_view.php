bcb0665248c75a880f9e05c3ae662da090b9b6b6
Mohammad <mohammad.trabelsi@gmail.com>
2024-12-18 11:10:05 +0100

    Update views

--- database/migrations/2024_11_12_102713_update_allbycountries_view.php
+++ database/migrations/2024_11_12_102713_update_allbycountries_view.php
@@ -8,6 +8,7 @@ return new class extends Migration
 
     public function up()
     {
+        dump(env('APP_NAME', '2Earn.test'));
         DB::statement(formatSqlWithEnv(getSqlFromPath('_update_allbycountries_view')));
     }
 
