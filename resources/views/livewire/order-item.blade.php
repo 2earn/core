1c33a24799e09457d1052ae8888b505e2f498eb9
Mohammad <mohammad.trabelsi@gmail.com>
2025-01-08 12:29:35 +0100

    Update order id ui

--- resources/views/livewire/order-item.blade.php
+++ resources/views/livewire/order-item.blade.php
@@ -1,7 +1,7 @@
 <div class="card border card-border-light">
     <div class="card-header">
         <h5 class="card-title mb-1">
-            {{$order->id}}
+            <span class="badge rounded-pill bg-primary">{{$order->id}}</span>
         </h5>
     </div>
     <div class="card-body">
