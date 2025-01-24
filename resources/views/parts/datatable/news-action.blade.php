9e9946b60ef741a5b81b4b26fe607bc1a341807d
Mohammad <mohammad.trabelsi@gmail.com>
2025-01-24 12:30:14 +0100

    Update News actions

--- resources/views/parts/datatable/news-action.blade.php
+++ resources/views/parts/datatable/news-action.blade.php
@@ -1,5 +1,7 @@
-<a href="{{route('news_create_update', ['locale' => app()->getLocale(), 'idNews' => $newsId])}}"
+@if(\App\Models\User::isSuperAdmin())
+    <a href="{{route('news_create_update', ['locale' => app()->getLocale(), 'idNews' => $newsId])}}"
    class="btn btn-xs btn-primary btn2earnTable  m-1">{{__('Edit')}}</a>
 
 <a data-id="{{$newsId}}" data-name="{{$newstitle }}" title="{{$newstitle }}"
    class="btn btn-xs btn-danger btn2earnTable deleteNews m-1">{{__('Delete')}}</a>
+@endif
