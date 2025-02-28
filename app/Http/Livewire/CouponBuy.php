e5a01163ca38b1342d8c0b2a9f898c83fc7cf5a0
Mohammad <mohammad.trabelsi@gmail.com>
2025-02-28 14:59:25 +0100

    Prepare coupons buy

--- app/Http/Livewire/CouponBuy.php
+++ app/Http/Livewire/CouponBuy.php
@@ -2,10 +2,42 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Coupon;
 use Livewire\Component;
 
 class CouponBuy extends Component
 {
+
+    public function getCouponsForAmount($amount)
+    {
+
+        $availableCoupons = Coupon::where('status', 'available')
+            ->orderBy('value', 'desc')
+            ->get();
+
+        $selectedCoupons = [];
+        $total = 0;
+
+        foreach ($availableCoupons as $coupon) {
+            if ($total + $coupon->value <= $amount) {
+                $selectedCoupons[] = $coupon;
+                $total += $coupon->value;
+            }
+
+            if ($total == $amount) {
+                break;
+            }
+        }
+
+        if ($total == $amount) {
+            return response()->json([
+                'amount' => $amount,
+                'coupons' => $selectedCoupons,
+            ]);
+        }
+        return null;
+    }
+
     public function render()
     {
         return view('livewire.coupon-buy')->extends('layouts.master')->section('content');
--- resources/views/livewire/coupon-buy.blade.php
+++ resources/views/livewire/coupon-buy.blade.php
@@ -1,11 +1,38 @@
 <div>
     @component('components.breadcrumb')
         @slot('title')
-            {{ __('Coupon buy') }}
+            {{ __('Coupon buy e') }}
         @endslot
     @endcomponent
     <div class="card">
         <div class="card-body">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="card">
+                        <div class="card-header align-items-center d-flex">
+                            <h4 class="card-title mb-0 flex-grow-1">{{__('Simulate amount of coupons')}}</h4>
+                        </div>
+                        <div class="card-body">
+                            <div class="live-preview">
+                                <div>
+                                    <div class="row g-3">
+                                        <div class="col-lg-12">
+                                            <div class="input-group">
+                                                <input type="text" class="form-control"
+                                                       aria-label="Recipient's username"
+                                                       aria-describedby="button-addon2">
+                                                <button class="btn btn-outline-success material-shadow-none"
+                                                        type="button" id="button-addon2">{{__('Simulate')}}
+                                                </button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </div>
