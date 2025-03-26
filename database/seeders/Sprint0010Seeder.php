27b8331ba5e820ab8a1981b567e2a9806358f2b9
Mohammad <mohammad.trabelsi@gmail.com>
2025-03-26 05:19:14 +0100

    Work sseeders

--- database/seeders/Sprint0010Seeder.php
+++ database/seeders/Sprint0010Seeder.php
@@ -5,13 +5,15 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Log;
 
 class Sprint0010Seeder extends Seeder
 {
 
     public function run($dataTranslation = false, $dataMoney = true, $dataDeal = true)
     {
+        Log::notice('Starting Seeder Sprint008Seeder');
         Artisan::call('db:seed', ['--class' => 'Database\Seeders\ItemCouponSeeder']);
-
+        Log::notice('Ending Seeder Sprint0010Seeder');
     }
 }
--- database/seeders/Sprint008Seeder.php
+++ database/seeders/Sprint008Seeder.php
@@ -4,13 +4,14 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Log;
 
 class Sprint008Seeder extends Seeder
 {
 
     public function run($dataTranslation = false, $dataMoney = true, $dataDeal = true)
     {
-
+        Log::notice('Starting Seeder Sprint008Seeder');
         Artisan::call('db:seed', ['--class' => 'Database\Seeders\BalanceOperationsSeeder']);
         Artisan::call('db:seed', ['--class' => 'Database\Seeders\RoleSeeder']);
         Artisan::call('db:seed', ['--class' => 'Database\Seeders\BalancesSeeder']);
@@ -22,6 +23,7 @@ class Sprint008Seeder extends Seeder
         Artisan::call('db:seed', ['--class' => 'Database\Seeders\BalancesSeeder']);
         Artisan::call('db:seed', ['--class' => 'Database\Seeders\BalancesSQLSeeder']);
         Artisan::call('db:seed', ['--class' => 'Database\Seeders\DeleteTriggers']);
+        Log::notice('Ending Seeder Sprint008Seeder');
 
     }
 }
