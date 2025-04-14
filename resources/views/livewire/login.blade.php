5e5ff82a408fc67260700af58f372ca35901d2fe
Mohammad <mohammad.trabelsi@gmail.com>
2025-04-14 13:59:03 +0100

    Update password show

--- resources/views/livewire/login.blade.php
+++ resources/views/livewire/login.blade.php
@@ -101,9 +101,13 @@
                                                     </label>
                                                     <label class="form-label"
                                                            for="password-input">{{ __('Password') }}</label>
-                                                                                <div class="position-relative auth-pass-inputgroup mb-3">
-                                                        <input type="password" class="form-control pe-5 password-input" placeholder="{{ __('Password') }}" id="password-input">
-                                                        <button class="btn btn-link position-absolute end-0 top-0 text-decoration-none text-muted password-addon material-shadow-none" type="button" id="password-addon"><i class="ri-eye-fill align-middle"></i></button>
+                                                    <div class="position-relative auth-pass-inputgroup mb-3">
+                                                        <input type="password" class="form-control pe-5 password-input"
+                                                               placeholder="{{ __('Password') }}" id="password-input">
+                                                        <button
+                                                            class="btn btn-link position-absolute end-0 top-0 text-decoration-none text-muted password-addon material-shadow-none"
+                                                            type="button" id="password-addon"><i
+                                                                class="ri-eye-fill align-middle"></i></button>
                                                         @error('password')
                                                         <span class="invalid-feedback" role="alert">
                                                                                                 <strong>{{ $message }}</strong>
@@ -213,7 +217,7 @@
             function initIntlTelInput() {
                 $("#signin").prop("disabled", false);
                 var phone = itiLog.getNumber();
-                 document.createTextNode(phone);
+                document.createTextNode(phone);
                 phone = phone.replace('+', '00');
                 var countryData = itiLog.getSelectedCountryData();
                 phone = '00' + countryData.dialCode + phone;
