45bfec1cb2ecafb02202853bab8ef1b102c1b162
Mohammad <mohammad.trabelsi@gmail.com>
2025-04-22 17:37:28 +0100

    Update coupons

--- app/Livewire/CouponBuy.php
+++ app/Livewire/CouponBuy.php
@@ -63,7 +63,7 @@ class CouponBuy extends Component
     public function BuyCoupon()
     {
         $platform = Platform::find($this->idPlatform);
-        $order = Order::create(['user_id' => auth()->user()->id, 'note' => 'Coupon buy from :' . $this->idPlatform . '-' . $platform->name]);
+        $order = Order::create(['user_id' => auth()->user()->id, 'note' => 'Coupon buy from' . ' :' . $this->idPlatform . '-' . $platform->name]);
         $coupon = Item::where('ref', '#0001')->where('platform_id', $this->idPlatform)->first();
         foreach ($this->coupons as $couponItem) {
             $order->orderDetails()->create([
