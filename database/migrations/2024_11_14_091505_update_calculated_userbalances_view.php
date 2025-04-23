7673f0f295e6cee1c13b26fcbdb7dd798b6a4454
Mohammad <mohammad.trabelsi@gmail.com>
2025-04-23 14:22:29 +0100

    Update Migration

--- database/migrations/2024_11_14_091505_update_calculated_userbalances_view.php
+++ database/migrations/2024_11_14_091505_update_calculated_userbalances_view.php
@@ -6,12 +6,13 @@ return new class extends Migration {
 
     public function up()
     {
-       // DB::statement(formatSqlWithEnv(getSqlFromPath('_update_calculated_userbalances_view')));
+        DB::statement('DROP VIEW IF EXISTS calculated_userbalances');
+        DB::statement('DROP VIEW IF EXISTS 	soldes_view');
+        DB::statement(formatSqlWithEnv(getSqlFromPath('_update_calculated_userbalances_view')));
     }
 
     public function down()
     {
         DB::statement('DROP VIEW IF EXISTS calculated_userbalances');
-
     }
 };
