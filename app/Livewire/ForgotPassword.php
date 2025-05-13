e1844ad73882b2ac94ea6ddb90468ad444f227f8
Mohammad <mohammad.trabelsi@gmail.com>
2025-05-13 15:03:53 +0100

    Fix Forgot Password attemps

--- app/Livewire/ForgotPassword.php
+++ app/Livewire/ForgotPassword.php
@@ -8,14 +8,20 @@ use Core\Enum\StatusRequest;
 use Core\Enum\TypeEventNotificationEnum;
 use Core\Enum\TypeNotificationEnum;
 use Core\Services\settingsManager;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Crypt;
 use Illuminate\Support\Facades\Lang;
+use Illuminate\Support\Facades\RateLimiter;
 use Livewire\Component;
 
 
 class ForgotPassword extends Component
 {
     use earnLog;
+
+    const MAX_ATTEMPTS = 3;
+    const COOLDOWN_PERIOD = 300;
+    const RATE_KEY = 'forgot-password-attempts-';
 
     public $locales;
     public $check;
@@ -28,10 +34,20 @@ class ForgotPassword extends Component
 
     public function PresendSms($ccode, $fullNumber, settingsManager $settingsManager)
     {
+        $key = self::RATE_KEY . $fullNumber;
+
+        if (RateLimiter::tooManyAttempts($key, self::MAX_ATTEMPTS)) {
+            Cache::put('blocked-user-' . $fullNumber, true, now()->addMinutes(10));
+            return redirect()->route("forget_password", app()->getLocale())
+                ->with('danger', Lang::get('Too many attempts! Please try again later.'));
+        }
+
+        RateLimiter::hit($key, self::COOLDOWN_PERIOD); // Set the cooldown period (e.g., 60 seconds)
+
         $user = $settingsManager->getUserByFullNumber($fullNumber);
         if (!$user) {
             $this->earnDebug('Forget password user not found : fullNumber- ' . $fullNumber . ' code pays- ' . $ccode);
-            return redirect()->route("forget_password", app()->getLocale())->with('danger', Lang::get('User not found !'));
+            return redirect()->route("forget_password", app()->getLocale())->with('danger', Lang::get('User not found'));
         }
         if ($user?->status == StatusRequest::Registred->value) {
             $this->earnDebug('Forget password user with not valid status : fullNumber- ' . $fullNumber . ' code pays- ' . $ccode);
@@ -64,6 +80,9 @@ class ForgotPassword extends Component
             $this->earnDebug('Forget password input opt code OPT invalide  : fullNumber- ' . $phoneNumber . ' codeOPT- ' . $codeOPT);
             return redirect()->route("forget_password", app()->getLocale())->with('danger', Lang::get('Invalid OPT code'));
         }
+
+        RateLimiter::clear(self::RATE_KEY . $phoneNumber);
+
         return redirect()->route("reset_password", ["locale" => app()->getLocale(), "idUser" => Crypt::encryptString($user->idUser)]);
     }
 
