a8e0e86c2b0e1293894879682a93955fbb3d3029
Mohammad <mohammad.trabelsi@gmail.com>
2025-06-01 21:36:55 +0100

    UserBalancesUpdater remouve lunch

--- database/seeders/Version4v2Seeder.php
+++ database/seeders/Version4v2Seeder.php
@@ -14,7 +14,6 @@ class Version4v2Seeder extends Seeder
     {
         Log::notice('Starting Seeder version 4.2');
         Artisan::call('db:seed', ['--class' => 'Database\Seeders\ActionSettingSeeder']);
-        Artisan::call('db:seed', ['--class' => 'Database\Seeders\UserBalancesUpdater']);
         Artisan::call('db:seed', ['--class' => 'Database\Seeders\UserBalancesValueUpdater']);
         Log::notice('Finish Seeder version 4.2');
     }
