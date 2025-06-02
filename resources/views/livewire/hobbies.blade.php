ad374f6bd3b9f959770fd9e6f505bb8aefa51d73
Mohammad <mohammad.trabelsi@gmail.com>
2025-06-02 12:14:25 +0100

    Fix container

--- resources/views/livewire/hobbies.blade.php
+++ resources/views/livewire/hobbies.blade.php
@@ -4,9 +4,11 @@
             {{ __('Hobbies') }}
         @endslot
     @endcomponent
+    <div class="container-fluid">
         <div class="card">
             <div class="card-body">
                 @livewire('page-timer', ['deadline'=>'hobbies_cs'])
             </div>
         </div>
+    </div>
 </div>
