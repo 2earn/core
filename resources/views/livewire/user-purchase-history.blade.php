13c53313716c90ad38539082d464b798da053230
Mohammad <mohammad.trabelsi@gmail.com>
2025-06-16 14:12:20 +0100

    init

--- resources/views/livewire/user-purchase-history.blade.php
+++ resources/views/livewire/user-purchase-history.blade.php
@@ -1,16 +1,14 @@
 <div class="container-fluid">
     @section('title')
-        {{ __('history') }}
+        {{ __('User purchase history') }}
     @endsection
     @component('components.breadcrumb')
-        @slot('li_1')@endslot
         @slot('title')
-            {{ __('history') }}
+                {{ __('User purchase history') }}
         @endslot
     @endcomponent
     <div class="card">
         <div class="card-body">
-            @livewire('page-timer', ['deadline' => 'purchases_history_cs'])
         </div>
     </div>
 </div>
