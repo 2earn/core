7a1812a7deb0c714fae5acf5d29c710262e83d32
Mohammad <mohammad.trabelsi@gmail.com>
2025-06-20 07:51:43 +0100

    add storage

--- app/Livewire/NewsCreateUpdate.php
+++ app/Livewire/NewsCreateUpdate.php
@@ -8,6 +8,7 @@ use App\Models\TranslaleModel;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Lang;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Storage;
 use Livewire\Component;
 use Livewire\WithFileUploads;
 
