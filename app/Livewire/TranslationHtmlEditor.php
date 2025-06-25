df234dcb4c66d73c6029d92e6489b4d1bc34227e
Mohammad <mohammad.trabelsi@gmail.com>
2025-06-25 12:25:59 +0100

    Update UI

--- app/Livewire/TranslationHtmlEditor.php
+++ app/Livewire/TranslationHtmlEditor.php
@@ -2,11 +2,42 @@
 
 namespace App\Livewire;
 
+use App\Models\TranslaleModel;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 use Livewire\Component;
 
 class TranslationHtmlEditor extends Component
 {
+    public $translateModel = '';
     public $content = '';
+    public $lang = '';
+    public $idT = '';
+
+    public function mount(Request $request)
+    {
+        $this->idT = Route::current()->parameter('id');
+        $this->lang = Route::current()->parameter('lang');
+        $this->flag = $this->lang;
+        if ($this->flag == 'ar') {
+            $this->flag = 'sa';
+        }
+        if ($this->flag == 'en') {
+            $this->flag = 'gb';
+        }
+
+        $this->translateModel = TranslaleModel::find($this->idT);
+
+        $this->content = match ($this->lang) {
+            'ar' => $this->translateModel->value,
+            'fr' => $this->translateModel->valueFr,
+            'en' => $this->translateModel->valueEn,
+            'es' => $this->translateModel->valueEs,
+            'tr' => $this->translateModel->valueTr,
+            'ru' => $this->translateModel->valueRu,
+            'de' => $this->translateModel->valueDe,
+        };
+    }
 
     public function render()
     {
--- resources/views/livewire/translation-html-editor.blade.php
+++ resources/views/livewire/translation-html-editor.blade.php
@@ -11,18 +11,129 @@
 
     <div class="card">
         <div class="card-header">
+            <ul class="list-group list-group-horizontal-md">
+                <li class="list-group-item">
+                    <a
+                        href="{{\App\Models\TranslaleModel::getLink($this->translateModel->name)}}">
+                                                                            <span
+                                                                                class="text-info">{{__('Go to the')}} </span>
+                    </a>
+                </li>
+                <li class="list-group-item">
+                    <img
+                        src="{{Vite::asset("resources/images/flags/" . strtolower($this->flag) . ".svg")}}"
+                        class="avatar-xs mx-2">
+                </li>
+                <li class="list-group-item">
+                    {{__('Class')}} : <span
+                        class="badge text-info">{{\App\Models\TranslaleModel::getClassNameFromName($this->translateModel->name)}}</span>
+                </li>
+                <li class="list-group-item">
+                    > {{__('Property')}} : <span
+                        class="badge text-info">{{\App\Models\TranslaleModel::getPropertyFromName($this->translateModel->name)}}</span>
+                </li>
+                <li class="list-group-item">
+                    > {{__('ID')}} : <span
+                        class="badge text-dark">{{\App\Models\TranslaleModel::getIdFromName($this->translateModel->name)}}</span>
+                </li>
+            </ul>
+
         </div>
         <div class="card-body">
             <div wire:ignore>
                 <textarea id="editor" wire:model.defer="content"></textarea>
             </div>
+
+            <ul class="list-group list-group-horizontal-md my-2">
+                <li class="list-group-item">
+                    <a href="{{route('translate_html',['locale'=>app()->getLocale(),'id'=>$idT,'lang'=>'en'])}}"
+                       id="add-item" class="btn btn-soft-secondary fw-medium float-end"><i
+                            class="ri-globe-fill"></i> {{__('HTML')}}
+                        <img
+                            src="{{Vite::asset("resources/images/flags/" . strtolower('gb') . ".svg")}}"
+                            alt="{{__('English')}}" title="{{__('English')}}"
+                            class="avatar-xxs mx-2">
+                    </a>
+                </li>
+                <li class="list-group-item">
+                    <a href="{{route('translate_html',['locale'=>app()->getLocale(),'id'=>$idT,'lang'=>'ar'])}}"
+                       id="add-item" class="btn btn-soft-secondary fw-medium float-end"><i
+                            class="ri-globe-fill"></i> {{__('HTML')}}
+                        <img
+                            src="{{Vite::asset("resources/images/flags/" . strtolower('sa') . ".svg")}}"
+                            alt="{{__('Arabe')}}" title="{{__('Arabe')}}"
+                            class="avatar-xxs mx-2">
+                    </a>
+                </li>
+                <li class="list-group-item">
+                    <a href="{{route('translate_html',['locale'=>app()->getLocale(),'id'=>$idT,'lang'=>'fr'])}}"
+                       id="add-item" class="btn btn-soft-secondary fw-medium float-end"><i
+                            class="ri-globe-fill"></i> {{__('HTML')}}
+                        <img
+                            src="{{Vite::asset("resources/images/flags/" . strtolower('fr') . ".svg")}}"
+                            alt="{{__('Francais')}}" title="{{__('Francais')}}"
+                            class="avatar-xxs mx-2">
+                    </a>
+                </li>
+                <li class="list-group-item">
+
+                    <a href="{{route('translate_html',['locale'=>app()->getLocale(),'id'=>$idT,'lang'=>'tr'])}}"
+                       id="add-item" class="btn btn-soft-secondary fw-medium float-end"><i
+                            class="ri-globe-fill"></i> {{__('HTML')}}
+                        <img
+                            src="{{Vite::asset("resources/images/flags/" . strtolower('tr') . ".svg")}}"
+                            alt="{{__('Turkish')}}" title="{{__('Turkish')}}"
+                            class="avatar-xxs mx-2">
+                    </a>
+                </li>
+                <li class="list-group-item">
+
+                    <a href="{{route('translate_html',['locale'=>app()->getLocale(),'id'=>$idT,'lang'=>'es'])}}"
+                       id="add-item" class="btn btn-soft-secondary fw-medium float-end"><i
+                            class="ri-globe-fill"></i> {{__('HTML')}}
+                        <img
+                            src="{{Vite::asset("resources/images/flags/" . strtolower('es') . ".svg")}}"
+                            alt="{{__('Spanish')}}" title="{{__('Spanish')}}"
+                            class="avatar-xxs mx-2">
+                    </a></li>
+                <li class="list-group-item">
+                    <a href="{{route('translate_html',['locale'=>app()->getLocale(),'id'=>$idT,'lang'=>'ru'])}}"
+                       id="add-item" class="btn btn-soft-secondary fw-medium float-end"><i
+                            class="ri-globe-fill"></i> {{__('HTML')}}
+                        <img
+                            src="{{Vite::asset("resources/images/flags/" . strtolower('ru') . ".svg")}}"
+                            alt="{{__('Russian')}}" title="{{__('Russian')}}"
+                            class="avatar-xxs mx-2">
+                    </a>
+                </li>
+                <li class="list-group-item">
+                    <a href="{{route('translate_html',['locale'=>app()->getLocale(),'id'=>$idT,'lang'=>'de'])}}"
+                       id="add-item" class="btn btn-soft-secondary fw-medium float-end"><i
+                            class="ri-globe-fill"></i> {{__('HTML')}}
+                        <img
+                            src="{{Vite::asset("resources/images/flags/" . strtolower('de') . ".svg")}}"
+                            alt="{{__('German')}}" title="{{__('German')}}"
+                            class="avatar-xxs mx-2">
+                    </a>
+                </li>
+            </ul>
+        </div>
+        <div class="card-footer">
+            <button type="button" class="btn btn-primary float-end material-shadow-none">
+                {{__('Save Translation')}}
+            </button>
         </div>
     </div>
     <script src="https://cdn.ckeditor.com/ckeditor5/27.1.0/classic/ckeditor.js"></script>
     <script type="module">
         document.addEventListener('DOMContentLoaded', function () {
             ClassicEditor
-                .create(document.querySelector('#editor'))
+                .create(document.querySelector('#editor'), {
+                    addPlugins: ['SourceEditing'], removePlugins: [
+                        'Image', 'ImageCaption', 'ImageStyle', 'ImageToolbar',
+                        'ImageUpload', 'MediaEmbed', 'CKFinder', 'CKFinderUploadAdapter', 'EasyImage'
+                    ],
+                })
                 .then(editor => {
                     editor.model.document.on('change:data', () => {
                     @this.set('content', editor.getData())
