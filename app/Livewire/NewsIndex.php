d0f5fabed8a737de4e819ef44246d0416322dddd
Mohammad <mohammad.trabelsi@gmail.com>
2025-07-16 13:09:18 +0100

    Replicate news

--- app/Livewire/NewsIndex.php
+++ app/Livewire/NewsIndex.php
@@ -2,11 +2,12 @@
 
 namespace App\Livewire;
 
-use Illuminate\Support\Facades\Route;
 use App\Models\News;
 use App\Models\News as ModelsNews;
+use App\Models\TranslaleModel;
 use Illuminate\Support\Facades\Lang;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Route;
 use Livewire\Component;
 use Livewire\WithPagination;
 
@@ -18,7 +19,7 @@ class NewsIndex extends Component
     public $search = '';
     public $currentRouteName;
     protected $paginationTheme = 'bootstrap';
-    public $listeners = ['delete' => 'delete'];
+    public $listeners = ['delete' => 'delete', 'duplicateNews' => 'duplicateNews'];
 
     public function mount()
     {
@@ -33,6 +34,40 @@ class NewsIndex extends Component
     public function updatingSearch(): void
     {
         $this->resetPage();
+    }
+
+    public function duplicateNews($id)
+    {
+        $original = News::findOrFail($id);
+        $duplicate = $original->replicate();
+        $duplicate->title = $original->title . ' (Copy)';
+        $duplicate->content = $original->content . ' (Copy)';
+        $duplicate->enabled = false;
+        $duplicate->created_at = now();
+        $duplicate->updated_at = now();
+        $duplicate->save();
+        $translations = ['title', 'content'];
+
+        foreach ($translations as $translation) {
+            TranslaleModel::create(
+                [
+                    'name' => TranslaleModel::getTranslateName($duplicate, $translation),
+                    'value' => $duplicate->{$translation} . ' AR',
+                    'valueFr' => $duplicate->{$translation} . ' FR',
+                    'valueEn' => $duplicate->{$translation} . ' EN',
+                    'valueTr' => $duplicate->{$translation} . ' TR',
+                    'valueEs' => $duplicate->{$translation} . ' ES',
+                    'valueRu' => $duplicate->{$translation} . ' Ru',
+                    'valueDe' => $duplicate->{$translation} . ' De',
+                ]);
+        }
+        if (!is_null($original->mainImage)) {
+            $image = $original->mainImage->replicate();
+            $image->imageable_id = $duplicate->id;
+            $image->save();
+        }
+        return redirect()->route('news_index', ['locale' => app()->getLocale()])->with('success', Lang::get('News Duplicated Successfully'));
+
     }
 
     public function delete($id)
