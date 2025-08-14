068f5424a1548ad6f6f77984532269b4097ef781
Mohammad <mohammad.trabelsi@gmail.com>
2025-08-14 09:33:11 +0100

    WIP mapping 3

--- database/migrations/2025_07_31_085403_update_balance_operations_table.php
+++ database/migrations/2025_07_31_085403_update_balance_operations_table.php
@@ -14,6 +14,7 @@ return new class extends Migration {
             $table->string('ref')->nullable()->unique();
             $table->unsignedBigInteger('operation_category_id')->foreign('operation_category_id')->nullable()->references('id')->on('operation_categories')->onDelete('cascade');
             // parent_operation_id ===>   parent_id
+            $table->enum('direction', ['IN', 'OUT']);
             $table->enum('balance_type', ['cash', 'bfs', 'discount', 'tree', 'sms', 'share', 'chance']);
             $table->boolean('relatable')->default(false);
             $table->string('relatable_model')->nullable();
