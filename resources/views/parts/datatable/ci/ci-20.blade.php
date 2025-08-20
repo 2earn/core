b081e87e3a6318814e1886951c5f76cf5cb55645
Mohammad <mohammad.trabelsi@gmail.com>
2025-08-20 16:21:48 +0100

    20 ci

--- resources/views/parts/datatable/ci/ci-20.blade.php
+++ resources/views/parts/datatable/ci/ci-20.blade.php
@@ -1,1 +1,43 @@
-<span class="text-info" title="{{$balance->balance_operation_id}}">{{$balance->id}}</span>
+@php
+    $idOrder=0;
+          $balanceModel=\App\Models\SharesBalances::find($balance->id);
+          $cashModel=\App\Models\CashBalances::where('reference',$balanceModel->reference)->where('balance_operation_id',\Core\Enum\BalanceOperationsEnum::OLD_ID_48->value)->first();
+@endphp
+@if(!is_null($cashModel))
+    <h5 class="text-muted">{{__('About the cash operation')}}:</h5>
+    <div data-simplebar style="max-height: 215px;">
+        <ul class="list-group">
+            <li class="list-group-item">
+                <div class="d-flex align-items-center">
+                    <div class="flex-grow-1">
+                        <div class="d-flex">
+                            <div class="flex-shrink-0 ms-2">
+                                <h6 class="fs-14 mb-0">{{__('Value')}}</h6>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="flex-shrink-0">
+                        <span class="text-danger">{{formatSolde($cashModel->value,2)}} {{config('app.currency')}}</span>
+                    </div>
+                </div>
+            </li>
+            <li class="list-group-item">
+                <div class="d-flex align-items-center">
+                    <div class="flex-grow-1">
+                        <div class="d-flex">
+                            <div class="flex-shrink-0 ms-2">
+                                <h6 class="fs-14 mb-0">{{__('Created at')}}</h6>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="flex-shrink-0">
+                        <span class="text-success">{{$cashModel->created_at}}</span>
+                    </div>
+                </div>
+            </li>
+
+        </ul>
+    </div>
+@else
+    <span class="text-muted">{{__('No cash operation found')}}:</span>
+@endif
