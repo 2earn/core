d3c95624002d7a29508ae06bec042c0d008eb7c8
Mohammad <mohammad.trabelsi@gmail.com>
2025-08-20 16:08:05 +0100

    16 ---> 19 ci

--- resources/views/parts/datatable/ci/ci-19.blade.php
+++ resources/views/parts/datatable/ci/ci-19.blade.php
@@ -1,2 +1,8 @@
-<span class="text-info" title="{{$balance->balance_operation_id}}">{{$balance->id}}</span>
-
+@php
+    $idOrder=0;
+          $balanceModel=\App\Models\SMSBalances::find($balance->id);
+        $idOrder=$balanceModel->order_id;
+@endphp
+<a title="{{$balance->balance_operation_id}}"
+   href="{{route('orders_detail', ['locale'=>app()->getLocale(),'id'=>$idOrder])}}"
+   class=float-end">{{__('Order details')}}</a>
