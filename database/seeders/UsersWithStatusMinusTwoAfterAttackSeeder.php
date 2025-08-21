a886e08059562a1e22968c27c499d8ccae1ce005
Mohammad <mohammad.trabelsi@gmail.com>
2025-08-21 08:34:18 +0100

    Update filter after attack

--- database/seeders/UsersWithStatusMinusTwoAfterAttackSeeder.php
+++ database/seeders/UsersWithStatusMinusTwoAfterAttackSeeder.php
@@ -5,18 +5,31 @@ namespace Database\Seeders;
 use App\Models\User;
 use Carbon\Carbon;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Log;
 
 class UsersWithStatusMinusTwoAfterAttackSeeder extends Seeder
 {
 
     public function run(): void
     {
+        $number = User::where('status', -2)
+            ->whereBetween('created_at', [
+                Carbon::create(2025, 7, 29)->startOfDay(),
+                Carbon::create(2025, 7, 30)->endOfDay()
+            ])->count();
+        Log::notice('user created from attack ' . $number);
         User::where('status', -2)
             ->whereBetween('created_at', [
                 Carbon::create(2025, 7, 29)->startOfDay(),
                 Carbon::create(2025, 7, 30)->endOfDay()
             ])
             ->delete();
+        $number = User::where('status', -2)
+            ->whereBetween('created_at', [
+                Carbon::create(2025, 7, 29)->startOfDay(),
+                Carbon::create(2025, 7, 30)->endOfDay()
+            ])->count();
+        Log::notice('user created from attack ' . $number);
 
     }
 }
