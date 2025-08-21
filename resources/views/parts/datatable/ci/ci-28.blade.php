cb4ca532e33a43df05b6310c98816dbab21db088
Mohammad <mohammad.trabelsi@gmail.com>
2025-08-21 07:59:55 +0100

    Raffine

--- resources/views/parts/datatable/ci/ci-28.blade.php
+++ resources/views/parts/datatable/ci/ci-28.blade.php
@@ -1,1 +1,43 @@
-<span class="text-info" title="{{$balance->balance_operation_id}}">{{$balance->id}}</span>
+@php
+          $balanceModel=\App\Models\DiscountBalances::find($balance->id);
+          $bfsModel=\App\Models\BFSsBalances::where('reference',$balanceModel->reference)
+          ->where('balance_operation_id',\Core\Enum\BalanceOperationsEnum::OLD_ID_13->value)->first();
+@endphp
+
+@if(!is_null($bfsModel))
+    <span class="text-muted" title="{{$bfsModel->reference}}">{{__('About the bfs operation')}}:</span>
+    <div data-simplebar style="max-height: 215px;">
+        <ul class="list-group">
+            <li class="list-group-item">
+                <div class="d-flex align-items-center">
+                    <div class="flex-grow-1">
+                        <div class="d-flex">
+                            <div class="flex-shrink-0 ms-2">
+                                <h6 class="fs-14 mb-0">{{__('Value')}}</h6>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="flex-shrink-0">
+                        <span class="text-danger">{{formatSolde($bfsModel->value,0)}}</span>
+                    </div>
+                </div>
+            </li>
+            <li class="list-group-item">
+                <div class="d-flex align-items-center">
+                    <div class="flex-grow-1">
+                        <div class="d-flex">
+                            <div class="flex-shrink-0 ms-2">
+                                <h6 class="fs-14 mb-0">{{__('Created at')}}</h6>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="flex-shrink-0">
+                        <span class="text-success">{{$bfsModel->created_at}}</span>
+                    </div>
+                </div>
+            </li>
+        </ul>
+    </div>
+@else
+    <span class="text-muted">{{__('No bfs operation found')}}</span>
+@endif
