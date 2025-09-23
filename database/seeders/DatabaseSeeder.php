24efb7adb1c0f460222065cc9375a6e86b61490c
Mohammad <mohammad.trabelsi@gmail.com>
2025-09-23 12:47:13 +0100

    Add db seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,13 +6,15 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
+
     public function run(): void
     {
         $this->call([
-            EventSeeder::class,
+            BalanceOperationsCouponSeeder::class,
+            BalanceOperationsBFSShareSeeder::class,
+            OperationCategorySeeder::class,
+            BalanceOperationsMappingSeeder::class,
+            UsersWithStatusMinusTwoAfterAttackSeeder::class
         ]);
     }
 }
