0c4d49064a00870ca66e7fe25e4f4d8efc5f7193
Mohammad <mohammad.trabelsi@gmail.com>
2025-10-06 15:30:25 +0100

    Implement help pages

--- resources/views/components/page-title.blade.php
+++ resources/views/components/page-title.blade.php
@@ -2,13 +2,19 @@
     <div class="col-auto">
         <a href="{{route('home', app()->getLocale())}}"
            title="{{__('To Home')}}">
-            <h6 class="breadcrumb-item text-white"><i class="ri-home-7-line"></i> </h6>
+            <h5 class="breadcrumb-item text-white"><i class="ri-home-7-line"></i></h5>
         </a>
     </div>
     @if($pageTitle!='Home')
-        <div class="col-auto">
-            <h6 class="breadcrumb-item text-white">{!! $pageTitle !!}</h6>
-        </div>
+        <span class="col align-self-center text-truncate">
+            <h6 class="breadcrumb-item text-white">{!! $pageTitle !!}
+            @if(!empty($helpUrl) && $helpUrl !== '#')
+                <a href="{{ $helpUrl }}" title="{{__('Check the help page')}}"
+                   class="m2 badge badge-outline-light float-end">
+                    <i class="ri-question-line text-white"></i>
+                </a>
+            @endif
+            </h6>
+        </span>
     @endif
 </div>
-
