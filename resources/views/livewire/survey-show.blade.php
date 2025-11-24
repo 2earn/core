031ad5733bcf3cf352fa9c5d70d5b719244ba5a5
Mohammad <mohammad.trabelsi@gmail.com>
2025-11-24 17:01:15 +0100

    Update survey UI

--- resources/views/livewire/survey-show.blade.php
+++ resources/views/livewire/survey-show.blade.php
@@ -1,13 +1,15 @@
-<div @class([$currentRouteName=="surveys_show" ?getContainerType():'row'])>
-    @component('components.breadcrumb')
-        @slot('title')
-            {{ __('Survey Show') }} : {{ $survey->id }}
-            - {{\App\Models\TranslaleModel::getTranslation($survey,'name',$survey->name)}}
-        @endslot
-    @endcomponent
-    @include('layouts.flash-messages')
-    @include('livewire.survey-item', ['survey' => $survey])
-    @if($survey->status==\Core\Enum\StatusSurvey::OPEN->value)
-        @vite('resources/js/surveys.js')
-    @endif
+<div>
+    <div @class([$currentRouteName=="surveys_show" ?getContainerType():'row'])>
+        @component('components.breadcrumb')
+            @slot('title')
+                {{ __('Survey Show') }} : {{ $survey->id }}
+                - {{\App\Models\TranslaleModel::getTranslation($survey,'name',$survey->name)}}
+            @endslot
+        @endcomponent
+        @include('layouts.flash-messages')
+        @include('livewire.survey-item', ['survey' => $survey])
+        @if($survey->status==\Core\Enum\StatusSurvey::OPEN->value)
+            @vite('resources/js/surveys.js')
+        @endif
+    </div>
 </div>
