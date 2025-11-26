c435f359a32ce8fa16b1e6236c2181b0e9ced630
Mohammad <mohammad.trabelsi@gmail.com>
2025-11-26 16:35:14 +0100

    Fix div

--- resources/views/livewire/change-password-page.blade.php
+++ resources/views/livewire/change-password-page.blade.php
@@ -14,8 +14,7 @@
             </div>
 
             <div class="row">
-                <div class="col-12">
-                    <div class="card shadow-sm">
+                    <div class="col-12 card shadow-sm">
                         <div class="card-header bg-transparent border-bottom">
                             <div class="d-flex align-items-center">
                                 <i class="ri-lock-password-line fs-4 text-info me-2"></i>
@@ -26,7 +25,6 @@
                             <livewire:change-password/>
                         </div>
                     </div>
-                </div>
             </div>
         </div>
     </div>
