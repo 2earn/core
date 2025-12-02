846638df9d0d54016c2b1467834bf0b8f2daaa45
Mohammad <mohammad.trabelsi@gmail.com>
2025-12-02 13:00:21 +0100

    update comm route

--- routes/api.php
+++ routes/api.php
@@ -123,7 +123,7 @@ Route::prefix('/partner/')->name('api_partner_')
             Route::apiResource('deals', DealPartnerController::class)->except('destroy');
             Route::apiResource('orders', OrderPartnerController::class)->except('destroy');
             Route::apiResource('order-details', OrderDetailsPartnerController::class)->only(['store', 'update']);
-            Route::get('deals/commission-formulas', [CommissionFormulaPartnerController::class, 'index'])->name('deals_commission_formulas_index');
+            Route::get('commission-formulas', [CommissionFormulaPartnerController::class, 'index'])->name('deals_commission_formulas_index');
             Route::patch('orders/{order}/status', [OrderPartnerController::class, 'changeStatus'])->name('orders_change_status');
             Route::patch('deals/{deal}/status', [DealPartnerController::class, 'changeStatus'])->name('deals_change_status');
             Route::post('items', [ItemsPartnerController::class, 'store'])->name('items_store');
